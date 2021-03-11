542ddcf73124d16405601061953ac3350ebc095c
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-10 22:32:02 -0500

    remove comments

--- calculator.js
+++ calculator.js
@@ -5,7 +5,6 @@ const clearBtn = document.getElementById("clear");
 const digits = document.getElementById("digits");
 const operation = document.getElementById("operation")
 
-//Click event for the clear button
 clearBtn.addEventListener("click", function (event) {
     event.preventDefault();
     numOne.value = "";
@@ -13,30 +12,24 @@ clearBtn.addEventListener("click", function (event) {
     digits.innerText = ""
 });
 
-//Click event for the equals button
 equalsBtn.addEventListener("click", function (event) {
     event.preventDefault();
 
-    //Checks if entries are numbers or empty
     if ((isNaN(numOne.value)) || (isNaN(numTwo.value))) {
         digits.innerText = "Error";
     } else if (numOne.value === "" && numTwo.value === "") {
         digits.innerText = "Error";
     } else if (numOne.value === "" || numTwo.value === "") {
         digits.innerText = "Error";
-        //checks for addition
     } else if (operation.value === "+") {
         let total = parseFloat(numOne.value) + parseFloat(numTwo.value);
         digits.innerText = total;
-        //checks for subtraction
     } else if (operation.value === "−") {
         let total = parseFloat(numOne.value) - parseFloat(numTwo.value);
         digits.innerText = total;
-        //checks for multiplication
     } else if (operation.value === "×") {
         let total = parseFloat(numOne.value) * parseFloat(numTwo.value);
         digits.innerText = total;
-        //checks for division
     } else if (operation.value === "÷") {
         let total = parseFloat(numOne.value) / parseFloat(numTwo.value);
         digits.innerText = total;
